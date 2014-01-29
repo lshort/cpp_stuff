d676f0e857ff34bab4d43486800e0cd19a08d489
Lee Short <blackcat@pro-ns.net>
2014-01-29 01:06:05 -0500

    first graph code

---
+++ graph.cpp
@@ -0,0 +1,42 @@
+/// graph.cpp
+///   some graph algorithms
+
+#include <string>
+
+using std::string;
+
+struct node;
+
+struct node_list {
+    node *data;
+    node_list *next;
+};
+
+
+struct node  {
+    node_list* connections;
+    string name;
+};
+
+class vertex {
+    vertex( string name ) : _name(name) {};
+    const string & getName() { return _name; };
+private:
+    string _name;
+};
+
+class edge {
+    edge( const vertex &from, const vertex &to) : _from(from), _to(to) {};
+    const vertex &getFrom() { return _from; };
+    const vertex &getTo() { return _to; };
+private:
+    const vertex &_from, &_to;
+};
+
+
+class digraph {
+public:
+
+private:
+
+};
