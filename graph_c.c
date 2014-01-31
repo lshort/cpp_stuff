400ad23e41fc8902ca114e16e96e45d12539c11d
Lee Short <blackcat@pro-ns.net>
2014-01-31 00:25:34 -0500

    better hack

--- graph_c.c
+++ graph_c.c
@@ -2,7 +2,7 @@
 ///   some graph algorithms
 
 #include <malloc.h>
-
+#include <stdio.h>
 
 struct node;
 
@@ -23,11 +23,14 @@ int main( int argc, char *argv[] ) {
     rval.vertices = malloc( size * sizeof(char*));
     rval.adj_list = malloc( size * sizeof(struct edge*));
     rval.vertices[0] = "A";
-    rval.adj_list[0] = { {"A","B",2},{"A","E",1} };
+    rval.adj_list[0] = (struct edge []){ {"A","B",2},{"A","E",1} };
     rval.vertices[1] = "B";
-    rval.adj_list[1] = { {"B","C",3},{"B","A",2} };
+    rval.adj_list[1] = (struct edge []){ {"B","C",3},{"B","A",2} };
     rval.vertices[2] = "C";
     rval.vertices[3] = "D";
     rval.vertices[4] = "E";
-
+    printf("%s", rval.vertices[0]);
+    free(rval.vertices);
+    free(rval.adj_list);
+    return 0;
 }
