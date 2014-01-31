9f78da84b68454b4dbf590dea67cc1f2f99a8a23
lshort <blackcat@pro-ns.net>
2014-01-30 16:13:06 -0800

    Next tranche

--- graph_c.c
+++ graph_c.c
@@ -1,31 +1,33 @@
 /// graph.cpp
 ///   some graph algorithms
 
-#include <string>
-#include <unordered_map>
+#include <malloc.h>
 
-using std::string;
-using std::unordered_map;
 
 struct node;
 
 struct edge {
-    node *from;
-    node *to;
-    int *weight;
-}
-
-struct edge_list {
-    edge *data;
-    node_list *next;
-};
-
-struct node  {
-    node_list *connections;
-    char *name;
+    char *from;
+    char *to;
+    int weight;
 };
 
 struct digraph {
-    char *vertices[];
-    edge_list *adj_lists;
+    char **vertices;
+    struct edge **adj_list;
 };
+
+int main( int argc, char *argv[] ) {
+    struct digraph rval;
+    int size = 5;
+    rval.vertices = malloc( size * sizeof(char*));
+    rval.adj_list = malloc( size * sizeof(struct edge*));
+    rval.vertices[0] = "A";
+    rval.adj_list[0] = { {"A","B",2},{"A","E",1} };
+    rval.vertices[1] = "B";
+    rval.adj_list[1] = { {"B","C",3},{"B","A",2} };
+    rval.vertices[2] = "C";
+    rval.vertices[3] = "D";
+    rval.vertices[4] = "E";
+
+}
