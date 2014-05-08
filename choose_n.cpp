e738b934263a3b4423fba7268d09ea2a403b15ca
lshort <blackcat@pro-ns.net>
2014-05-08 15:43:19 -0700

    better algorithm

--- choose_n.cpp
+++ choose_n.cpp
@@ -9,78 +9,64 @@ using namespace std;
 
 
 /**  Randomly choose n members of the input vector.  This runs
-     in O(n^2) time.
+     in O(n) time and uses O(1) space.
      @param[in] data The input vector
      @param[in] n How many members to select
      @return A vector of the chosen members  */
-vector<int> choose_n_big_input(const vector<int> &data, int n)
+vector<int> choose_n(vector<int> &data, int n)
 {
     int size = data.size();
     if (n>=size)
         return vector<int>(data.begin(),data.end());
-
-    vector<int> rval;
-    vector<int> used_indices; // this is kept sorted
-    for (int i=0; i<n; ++i)  {
-        int target = rand() % (size-i);
-        int offset = 0;
-        auto it = used_indices.begin();
-        while (it!=used_indices.end() && *it <= target+offset) {
-            ++offset;
-            ++it;
-        }
-        used_indices.emplace(it, target+offset);
-        rval.push_back(data[target+offset]);
-    }
-    return rval;
-}
-
-/**  Randomly choose n members of the input vector.  This dispatches
-     to choose_n_big_input if the size of the input is more than n^2.
-     If not dispatched, runs in time linear with the length of the vector
-     @param[in] data The input vector
-     @param[in] n How many members to select
-     @return A vector of the chosen members  */
-vector<int> choose_n(const vector<int> &data, int n)
-{
-    int size = data.size();
-    if (n>=size)
-        return vector<int>(data.begin(),data.end());
-    if (n*n<size)
-        return choose_n_big_input(data, n);
-    vector<int> tmp(data);
-    for (int i=1; i<size; ++i) {
-        int targ = rand() % (1+i);
-        std::swap(tmp[i], tmp[targ]);
-    }
-    return vector<int>(tmp.begin(),tmp.begin()+n);
-}
-
-/**  Randomly choose n members of the input vector.  Reorders the
-     input vector as a destructive side effect.
-     @param[in] data The input vector
-     @param[in] n How many members to select
-     @return A vector of the chosen members  */
-vector<int> choose_n_destructive(vector<int> &data, int n)
-{
-    int size = data.size();
-    if (n>=size)
-        return vector<int>(data.begin(),data.end());
+    vector<int> rval(n,0);
+    // on the first pass rval has the order of the swaps performed,
+    // later we will fill it with the return values
     for (int i=0; i<n; ++i) {
         int target = i + (rand()%(size-i));
+        rval[i] = target;
         std::swap(data[i], data[target]);
     }
-    return vector<int>(data.begin(),data.begin()+n);
+    //  the first n elements of data have been randomly selected,
+    //  now we need to move them into place
+    for (int k=n-1; k>=0; --k) {
+        int val = data[k];
+        std::swap(data[k],data[rval[k]]);
+        rval[k] = val;
+    }
+    return move(rval);
+};
+
+
+
+/**  Prints out any container of any class, provided the class has operator <<
+     @param[in] ostr The output stream
+     @param[in] xs The container
+     @return Returns the output stream      */
+template< typename T,
+          template<typename El,
+                   typename Alloc=std::allocator<El> > class Container >
+ostream &operator<< (ostream &ostr, const Container<T> &xs)
+{
+    ostr << "{ ";
+    for ( auto &x : xs )
+        ostr << x << ",";
+    ostr << " }";
+    return ostr;
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
     vector<int> d = {1,2,3,4,5,6,7,8,9,10};
-    auto s = choose_n(d, 3);
-    cout << s.size() << endl;
-    for ( auto m : s )
-        cout << m << " ";
-    cout << endl;
 
+    vector<unsigned int> counts(11,0);
+
+    for (int i=0; i<atoi(argv[1]); ++i)  {
+        auto s = choose_n(d, 3);
+        ++counts[s[0]-1];
+        ++counts[s[1]-1];
+        ++counts[s[2]-1];
+    }
+    cout << "******** counts ********" << endl;
+    cout << counts << endl;
 }
