a0f1121b9c413e08add43dc25ed0befa9c28a0f0
lshort <blackcat@pro-ns.net>
2014-05-22 15:44:41 -0700

    with broken template in use

--- practice/lambdas.cpp
+++ practice/lambdas.cpp
@@ -6,17 +6,13 @@
 #include <cmath>
 #include <ctgmath>
 
-using namespace std;
-
-auto add = [](auto a, auto b) { return a+b; };
-auto sub = [](auto a, auto b) { return a-b; };
-auto mul = [](auto a, auto b) { return a*b; };
-auto dvd = [](auto a, auto b) { return a/b; };
+using std::cout;
+using std::endl;
+using std::vector;
 
 auto incr = [](auto a) { return a+1; };
 auto dbl = [](auto a) { return a*2; };
 auto triple = [](auto a) { return a*3; };
-//auto show = [](auto a) { cout << a << endl; };
 
 namespace functional {
     auto map = [] (const auto &fcn, const auto &vec )
@@ -38,30 +34,49 @@ namespace mutates {
 }
 
 namespace compose {
+    // works
     template<typename Lambda1, typename Lambda2>
     auto operator* (Lambda1 l1, Lambda2 l2)
     {
         return [l1, l2](auto x) { return l1(l2(x)); };
     };
+
+    // doesn't work
+    template <typename return_type, typename intermediate, typename base>
+    auto operator* (return_type (*f1)(intermediate),
+                    intermediate (*f2)(base))
+    {
+        return [f1, f2] (auto x) { return f1(f2(x)); };
+    };
 }
 
 using compose::operator*;
 
+int add1(int x)  { return x+1; }
+int sub1(int x)  { return x-1; }
+
 int main()
 {
     vector<int> data {1,2,3,4};
-    auto incd = functional::map(incr,data);
+    vector<int> incd = functional::map(incr,data);
     cout << incd[0] << " " << incd[3] << endl;
-    auto double_and_inc = incr * dbl;
-    auto double_inc_triple = triple * incr * dbl;
-    auto hacked = functional::map(double_and_inc,data);
-    cout << hacked[0] << " " << hacked[3] << endl;
-    int result = double_inc_triple(5);
-    cout << result << endl;
-    mutates::tmap(double_and_inc,data);
+    auto double_and_incr = incr * dbl;
+    auto double_incr_triple = triple * incr * dbl;
+
+    vector<int> map_results = functional::map(double_and_incr,data);
+    cout << map_results[0] << " " << map_results[3] << endl;
+    cout << double_incr_triple(5) << endl;
+
+    mutates::tmap(double_and_incr,data);
     cout << data[0] << " " << data[3] << endl;
-    cout << 3*5 << endl;
-    //  *** code below will not compile -- template operator* only takes lambdas
-    // auto ident = cos * incd;
-    // cout << ident(0.1) << endl;
+    cout << 3*5 << endl;   // make sure we haven't hidden plain old *
+
+    //  *** code below will not compile 
+    //      compose::operator* (function ptrs) does not resolve to the template
+    //      on line A (even if I remove op* from the namespace for lookup)
+    //      
+    int (*fp1)(int) = &add1;
+    int (*fp2)(int) = &sub1;
+    int (*ident)(int) = fp1 * fp2;     // A
+    cout << ident(1) << endl;
 }
